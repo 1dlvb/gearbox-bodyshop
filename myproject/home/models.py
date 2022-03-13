6207381c985583d86af9abdc7ef74335cf1413ca
1dlvb <matushkin-2017@mail.ru>
2022-03-13 14:34:02 +0400

    Added new wagtail features. Added gearbox homepage.

--- myproject/home/models.py
+++ myproject/home/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -13,9 +14,28 @@ class HomePage(Page):
     max_count = 1
 
     banner_title = models.CharField(max_length=200, blank=False, null=True)
+    banner_subtitle = RichTextField(features=['bold', 'italic'])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('banner_title')
+        FieldPanel('banner_title'),
+        FieldPanel('banner_subtitle'),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta")
     ]
 
     class Meta:
